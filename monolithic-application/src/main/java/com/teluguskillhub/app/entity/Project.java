7a57d14ef764ebb14ccd45bc285e9420cb53ac7c
Surendar <batthulasurendar@gmail.com>
2024-07-19 12:05:01 +0530

    implemented project related rest api's

--- monolithic-application/src/main/java/com/teluguskillhub/app/entity/Project.java
+++ monolithic-application/src/main/java/com/teluguskillhub/app/entity/Project.java
@@ -1,16 +1,22 @@
 package com.teluguskillhub.app.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "project_tb", uniqueConstraints = {@UniqueConstraint(columnNames = "projectCode")})
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long projectId;
-    @Column(nullable = false)
+    @Column(name = "project_code", nullable = false)
     private long projectCode;
-    @Column(nullable = false)
+    @Column(name = "project_name", nullable = false)
     private String projectName;
 
 }
--- monolithic-application/src/main/java/com/teluguskillhub/app/repository/ProjectRepo.java
+++ monolithic-application/src/main/java/com/teluguskillhub/app/repository/ProjectRepo.java
@@ -4,4 +4,5 @@ import com.teluguskillhub.app.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepo extends JpaRepository<Project, Long> {
+    Project findByProjectCode (long projectCode);
 }
