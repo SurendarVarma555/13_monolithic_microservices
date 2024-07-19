2acf2943bee1c0ba2fae5eafcb9b877ca69c03d4
Surendar <batthulasurendar@gmail.com>
2024-07-19 13:28:05 +0530

    implemented employee related rest api's

--- monolithic-application/src/main/java/com/teluguskillhub/app/service/EmployeeService.java
+++ monolithic-application/src/main/java/com/teluguskillhub/app/service/EmployeeService.java
@@ -1,10 +1,11 @@
 package com.teluguskillhub.app.service;
 
+import com.teluguskillhub.app.entity.Employee;
 import com.teluguskillhub.app.playload.EmployeeDto;
 
 public interface EmployeeService {
 
-    public EmployeeDto saveEmployee (EmployeeDto employeeDto);
+    public EmployeeDto saveEmployee (Employee employee);
 
     public EmployeeDto getEmployeeById (long empId);
 }
--- monolithic-application/src/main/java/com/teluguskillhub/app/service/Impl/EmployeeServiceImpl.java
+++ monolithic-application/src/main/java/com/teluguskillhub/app/service/Impl/EmployeeServiceImpl.java
@@ -1,18 +1,57 @@
 package com.teluguskillhub.app.service.Impl;
 
+import com.teluguskillhub.app.entity.Employee;
+import com.teluguskillhub.app.entity.Project;
 import com.teluguskillhub.app.playload.EmployeeDto;
+import com.teluguskillhub.app.repository.EmployeeRepo;
+import com.teluguskillhub.app.repository.ProjectRepo;
 import com.teluguskillhub.app.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @Autowired
+    private ProjectRepo projectRepo;
+
     @Override
-    public EmployeeDto saveEmployee (EmployeeDto employeeDto){
-        return null;
+    public EmployeeDto saveEmployee (Employee employee){
+
+        Employee savedEmployee = employeeRepo.save(employee);
+        Project project = projectRepo.findByProjectCode(savedEmployee.getEmployeeAssignedProject());
+
+        EmployeeDto employeeDto = new EmployeeDto();
+
+        employeeDto.setEmpId(savedEmployee.getEmpId());
+        employeeDto.setEmpName(savedEmployee.getEmpName());
+        employeeDto.setEmpEmail(savedEmployee.getEmpEmail());
+        employeeDto.setEmpBaseLocation(savedEmployee.getEmpBaseLocation());
+        employeeDto.setProjectCode(project.getProjectCode());
+        employeeDto.setProjectName(project.getProjectName());
+
+        return employeeDto;
     }
 
     @Override
     public EmployeeDto getEmployeeById (long empId){
-        return null;
+
+        Employee savedEmployee = employeeRepo.findById(empId).get();
+        Project project = projectRepo.findByProjectCode(savedEmployee.getEmployeeAssignedProject());
+
+        EmployeeDto employeeDto = new EmployeeDto();
+
+        employeeDto.setEmpId(savedEmployee.getEmpId());
+        employeeDto.setEmpName(savedEmployee.getEmpName());
+        employeeDto.setEmpEmail(savedEmployee.getEmpEmail());
+        employeeDto.setEmpBaseLocation(savedEmployee.getEmpBaseLocation());
+        employeeDto.setProjectCode(project.getProjectCode());
+        employeeDto.setProjectName(project.getProjectName());
+
+
+        return employeeDto;
     }
 }
