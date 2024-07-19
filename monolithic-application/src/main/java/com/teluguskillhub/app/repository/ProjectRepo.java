4fb9dcaeae789db2b35abfb7a1e1e01fe11657e5
Surendar <batthulasurendar@gmail.com>
2024-07-19 10:54:08 +0530

    added required repository;s

---
+++ monolithic-application/src/main/java/com/teluguskillhub/app/repository/ProjectRepo.java
@@ -0,0 +1,7 @@
+package com.teluguskillhub.app.repository;
+
+import com.teluguskillhub.app.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepo extends JpaRepository<Project, Long> {
+}
